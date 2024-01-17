63a7104f886efc74d58994b4df6dcf3a033a0f92
Alex Pate <git@alexjpate.com>
2024-01-17 20:39:45 +0100

    Remove unused auth tables

---
+++ prisma/migrations/20240117193852_remove_unused_auth_tables/migration.sql
@@ -0,0 +1,12 @@
+/*
+  Warnings:
+
+  - You are about to drop the `Session` table. If the table is not empty, all the data it contains will be lost.
+  - You are about to drop the `VerificationRequest` table. If the table is not empty, all the data it contains will be lost.
+
+*/
+-- DropTable
+DROP TABLE "Session";
+
+-- DropTable
+DROP TABLE "VerificationRequest";
