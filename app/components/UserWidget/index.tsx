afb0ed36c897cfb8dc5e821d1c7a294aff116a5f
Alex Pate <git@alexjpate.com>
2024-02-01 17:19:38 +0100

    Fix alignment of user nav

--- app/components/UserWidget/index.tsx
+++ app/components/UserWidget/index.tsx
@@ -50,7 +50,7 @@ export function UserWidget({ user, usersPages }: Props) {
             </Avatar>
           </Button>
         </DropdownMenuTrigger>
-        <DropdownMenuContent className="w-56" align="end" forceMount>
+        <DropdownMenuContent className="w-56" align="start" forceMount>
           <DropdownMenuLabel className="font-normal">
             <div className="flex flex-col space-y-1">
               <p className="text-sm font-medium leading-none">{user.name}</p>
