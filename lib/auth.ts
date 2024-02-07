e21f3faf515de92d25c5fcc367f6bec0103d6c9e
Alex Pate <git@alexjpate.com>
2024-02-07 18:05:31 +0100

    Add 2.0

--- lib/auth.ts
+++ lib/auth.ts
@@ -17,6 +17,7 @@ export const authOptions: NextAuthOptions = {
     TwitterProvider({
       clientId: process.env.TWITTER_CLIENT_ID as string,
       clientSecret: process.env.TWITTER_CLIENT_SECRET as string,
+      version: '2.0', // opt-in to Twitter OAuth 2.0
     }),
   ],
   secret: process.env.NEXTAUTH_SECRET,
