9a40907f519e3dbf96db5b473952175886972f5a
Alex Pate <git@alexjpate.com>
2024-02-10 11:52:19 +0100

    Make login widget closable

--- app/components/LoginWidget/index.tsx
+++ app/components/LoginWidget/index.tsx
@@ -3,14 +3,14 @@
 import { useState } from 'react';
 
 import {
-  AlertDialog,
-  AlertDialogContent,
-  AlertDialogDescription,
-  AlertDialogFooter,
-  AlertDialogHeader,
-  AlertDialogTitle,
-  AlertDialogTrigger,
-} from '@/components/ui/alert-dialog';
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from '@/components/ui/dialog';
 
 import { LoginProviderButton } from '../LoginProviderButton';
 
@@ -21,20 +21,18 @@ interface Props {
 export function LoginWidget({ trigger }: Props) {
   const [open, setOpen] = useState(false);
   return (
-    <AlertDialog open={open} onOpenChange={setOpen}>
-      <AlertDialogTrigger asChild>{trigger}</AlertDialogTrigger>
-      <AlertDialogContent>
-        <AlertDialogHeader>
-          <AlertDialogTitle>Welcome back!</AlertDialogTitle>
-          <AlertDialogDescription>
-            Login to your account below.
-          </AlertDialogDescription>
-        </AlertDialogHeader>
-        <AlertDialogFooter>
+    <Dialog open={open} onOpenChange={setOpen}>
+      <DialogTrigger asChild>{trigger}</DialogTrigger>
+      <DialogContent>
+        <DialogHeader>
+          <DialogTitle>Welcome back!</DialogTitle>
+          <DialogDescription>Login to your account below.</DialogDescription>
+        </DialogHeader>
+        <DialogFooter>
           <LoginProviderButton provider="twitter" className="mt-2 md:mt-0" />
           <LoginProviderButton provider="google" />
-        </AlertDialogFooter>
-      </AlertDialogContent>
-    </AlertDialog>
+        </DialogFooter>
+      </DialogContent>
+    </Dialog>
   );
 }
