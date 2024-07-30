99275124fd332cfca97200143f29f5ba9f1efabf
Alex Pate <git@alexjpate.com>
2024-07-30 22:17:59 +0200

    Add GitHub link on mobile

--- app/components/MarketingNavigation/index.tsx
+++ app/components/MarketingNavigation/index.tsx
@@ -102,6 +102,12 @@ export default function MarketingNavigation({ children }: Props) {
                   >
                     Twitter / X
                   </Link>
+                  <Link
+                    href="https://github.com/alexpate/glow"
+                    className="-mx-3 block rounded-lg px-3 py-2.5 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
+                  >
+                    View Source on GitHub
+                  </Link>
                 </div>
               </div>
             </div>
