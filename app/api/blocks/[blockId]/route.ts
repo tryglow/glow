e4bd084cce2da3469295db7feadfa61e7d8e0285
Alex Pate <git@alexjpate.com>
2024-10-16 18:54:02 +0200

    Prevent blocks from being fetched if page is not published

--- app/api/blocks/[blockId]/route.ts
+++ app/api/blocks/[blockId]/route.ts
@@ -1,9 +1,12 @@
+import { auth } from '@/lib/auth';
 import prisma from '@/lib/prisma';
 
 export async function GET(
   req: Request,
   { params }: { params: { blockId: string } }
 ) {
+  const session = await auth();
+
   const blockId = params.blockId;
 
   if (!blockId) {
@@ -16,12 +19,34 @@ export async function GET(
     where: {
       id: blockId,
     },
+    include: {
+      page: {
+        select: {
+          publishedAt: true,
+          teamId: true,
+        },
+      },
+    },
   });
 
+  if (!block?.page.publishedAt) {
+    if (session?.currentTeamId !== block?.page.teamId) {
+      return Response.json(
+        {},
+        {
+          status: 404,
+        }
+      );
+    }
+  }
+
   if (!block) {
-    return Response.json(null, {
-      status: 404,
-    });
+    return Response.json(
+      {},
+      {
+        status: 404,
+      }
+    );
   }
 
   return Response.json(block.data);
