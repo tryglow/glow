c00c944a2c511680da26092f3cb4c9ebb1afcc57
Alex Pate <git@alexjpate.com>
2024-10-17 22:50:54 +0200

    Add verification token model to DB

---
+++ prisma/migrations/20241017203139_add_verification_token_for_login_with_email/migration.sql
@@ -0,0 +1,12 @@
+-- CreateTable
+CREATE TABLE "VerificationToken" (
+    "identifier" TEXT NOT NULL,
+    "token" TEXT NOT NULL,
+    "expires" TIMESTAMP(3) NOT NULL
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "VerificationToken_token_key" ON "VerificationToken"("token");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "VerificationToken_identifier_token_key" ON "VerificationToken"("identifier", "token");
