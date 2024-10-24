30e9659f90ab153bde94e6cc0d63701b2642753e
Alex Pate <git@alexjpate.com>
2024-10-24 11:53:02 +0200

    Add sitemap

---
+++ app/sitemap.ts
@@ -0,0 +1,84 @@
+import { getArticles } from '@/app/i/blog/utils';
+import { getLearnPosts } from '@/app/i/learn/utils';
+import { MetadataRoute } from 'next';
+
+const baseUrl = `https://glow.as`;
+
+const baseSitemap = [
+  {
+    url: baseUrl,
+    lastModified: new Date(),
+    changeFrequency: 'weekly',
+    priority: 1,
+  },
+  {
+    url: `${baseUrl}/i/pricing`,
+    lastModified: new Date(),
+    changeFrequency: 'monthly',
+    priority: 0.7,
+  },
+  {
+    url: `${baseUrl}/i/terms`,
+    lastModified: new Date(),
+    changeFrequency: 'monthly',
+    priority: 0.4,
+  },
+  {
+    url: `${baseUrl}/i/privacy`,
+    lastModified: new Date(),
+    changeFrequency: 'monthly',
+    priority: 0.4,
+  },
+];
+export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
+  const blogSitemap = await generateBlogSitemap(baseUrl);
+  const learnSitemap = await generateLearnSitemap(baseUrl);
+
+  return [
+    ...baseSitemap,
+    ...blogSitemap,
+    ...learnSitemap,
+  ] as MetadataRoute.Sitemap;
+}
+
+const generateBlogSitemap = async (baseUrl: string) => {
+  const blogPosts = await getArticles();
+
+  const blogSitemap = blogPosts.map((post) => ({
+    url: `${baseUrl}/i/blog/${post.slug}`,
+    lastModified: new Date(post.publishDate),
+    changeFrequency: 'monthly',
+    priority: 0.5,
+  }));
+
+  return [
+    {
+      url: `${baseUrl}/i/blog`,
+      lastModified: new Date(),
+      changeFrequency: 'monthly',
+      priority: 0.5,
+    },
+    ...blogSitemap,
+  ];
+};
+
+const generateLearnSitemap = async (baseUrl: string) => {
+  const posts = await getLearnPosts();
+
+  const postsSitemap = posts.map((post) => ({
+    url: `${baseUrl}/i/learn/${post.slug}`,
+    lastModified: new Date(post.publishDate),
+    changeFrequency: 'monthly',
+    priority: 0.5,
+  }));
+
+  return [
+    {
+      url: `${baseUrl}/i/learn`,
+      lastModified: new Date(),
+      changeFrequency: 'monthly',
+      priority: 0.5,
+    },
+    ...postsSitemap,
+  ];
+};
