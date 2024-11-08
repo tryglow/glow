0187731553f32ef071181f296a77d51556d9a5ac
Alex Pate <git@alexjpate.com>
2024-11-08 18:22:22 +0100

    Revert prisma

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,18 +1,7 @@
-// import 'server-only';
-
 import { PrismaClient } from '@prisma/client';
-import { withAccelerate } from '@prisma/extension-accelerate';
-import { withOptimize } from '@prisma/extension-optimize';
 
 const prismaClientSingleton = () => {
-  if (!process.env.PRISMA_OPTIMIZE_API_KEY) {
-    console.warn('Prisma Optimize is not enabled');
-    return new PrismaClient();
-  }
-
-  return new PrismaClient()
-    .$extends(withOptimize({ apiKey: process.env.PRISMA_OPTIMIZE_API_KEY }))
-    .$extends(withAccelerate());
+  return new PrismaClient();
 };
 
 type PrismaClientSingleton = ReturnType<typeof prismaClientSingleton>;
