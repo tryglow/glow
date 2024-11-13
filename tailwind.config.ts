ccab530d0e2efcdd871cfeee5622f2f55249128f
Alex Pate <git@alexjpate.com>
2024-11-13 15:39:37 +0100

    Add slower spin animation

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -107,6 +107,7 @@ const config = {
       animation: {
         'accordion-down': 'accordion-down 0.2s ease-out',
         'accordion-up': 'accordion-up 0.2s ease-out',
+        'spin-slow': 'spin 4s linear infinite',
       },
     },
   },
