f18379ea02552c8085349001a72e891abd793b0e
Alex Pate <git@alexjpate.com>
2024-11-18 21:56:40 +0100

    Re-add prisma accelerate

--- app.frontend/lib/prisma.ts
+++ app.frontend/lib/prisma.ts
@@ -1,8 +1,17 @@
+import { withAccelerate } from '@prisma/extension-accelerate';
+import { withOptimize } from '@prisma/extension-optimize';
 import { PrismaClient } from '@tryglow/prisma';
 import 'server-only';
 
 const prismaClientSingleton = () => {
-  return new PrismaClient();
+  if (!process.env.PRISMA_OPTIMIZE_API_KEY) {
+    console.warn('Prisma Optimize is not enabled');
+    return new PrismaClient().$extends(withAccelerate());
+  }
+
+  return new PrismaClient()
+    .$extends(withOptimize({ apiKey: process.env.PRISMA_OPTIMIZE_API_KEY }))
+    .$extends(withAccelerate());
 };
 
 type PrismaClientSingleton = ReturnType<typeof prismaClientSingleton>;
