2b48484466320dea19000be17783d5cde087ae8d
Alex Pate <git@alexjpate.com>
2025-02-25 18:25:13 +0100

    Include creds in session check

--- app.marketing/src/hooks/use-is-logged-in.ts
+++ app.marketing/src/hooks/use-is-logged-in.ts
@@ -9,7 +9,13 @@ export function useIsLoggedIn() {
     const fetchSession = async () => {
       try {
         const req = await fetch(
-          `${process.env.NEXT_PUBLIC_API_URL}/session/me`
+          `${process.env.NEXT_PUBLIC_API_URL}/session/me`,
+          {
+            headers: {
+              'Content-Type': 'application/json',
+            },
+            credentials: 'include',
+          }
         );
 
         const res = await req.json();
