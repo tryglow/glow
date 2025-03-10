ca3fd23f4b049ff1d68dce8824e3e21cfc4ea38d
Alex Pate <git@alexjpate.com>
2025-03-10 19:20:33 +0100

    Fix up invites

--- app.frontend/app/invite/[inviteId]/accept-ui.tsx
+++ app.frontend/app/invite/[inviteId]/accept-ui.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { acceptInvite } from './actions';
+import { authClient } from '@/app/lib/auth';
 import { LoginForm } from '@/components/LoginForm';
 import { Button, toast } from '@tryglow/ui';
 import { useRouter } from 'next/navigation';
@@ -9,12 +9,14 @@ export function LoggedInAcceptInviteUI({ inviteId }: { inviteId: string }) {
   const router = useRouter();
 
   const handleAcceptInvite = async () => {
-    const { error } = await acceptInvite(inviteId);
+    const { error } = await authClient.organization.acceptInvitation({
+      invitationId: inviteId,
+    });
 
     if (error) {
       toast({
         title: 'Sorry, there was an error accepting your invite',
-        description: error,
+        description: error.message,
       });
 
       return;
@@ -27,5 +29,9 @@ export function LoggedInAcceptInviteUI({ inviteId }: { inviteId: string }) {
 }
 
 export function LoggedOutAcceptInviteUI({ inviteId }: { inviteId: string }) {
-  return <LoginForm redirectTo={`/i/invite/${inviteId}/accept`} />;
+  return (
+    <LoginForm
+      redirectTo={`${process.env.NEXT_PUBLIC_BASE_URL}/invite/${inviteId}`}
+    />
+  );
 }
