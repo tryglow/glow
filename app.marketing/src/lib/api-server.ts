673ce14f8ececd724f36b99ae25411c4de394dfc
Alex Pate <git@alexjpate.com>
2025-03-16 11:58:01 +0100

    Force var

--- app.marketing/src/lib/api-server.ts
+++ app.marketing/src/lib/api-server.ts
@@ -10,7 +10,12 @@ export const apiServerFetch = async (
     'x-api-key': process.env.INTERNAL_API_KEY,
   };
 
-  return fetch(`${process.env.NEXT_PUBLIC_API_URL}${path}`, {
+  console.log(
+    'process.env.NEXT_PUBLIC_API_URL',
+    process.env.NEXT_PUBLIC_API_URL
+  );
+
+  return fetch(`https://api.lin.ky${path}`, {
     headers,
     body: requestOptions.body,
     ...requestOptions,
