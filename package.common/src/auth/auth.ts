f5a2eb96a4c4cbf6402ce5af430f0c09550577a6
Alex Pate <git@alexjpate.com>
2025-03-18 19:41:10 +0100

    Cleanup

--- package.common/src/auth/auth.ts
+++ package.common/src/auth/auth.ts
@@ -8,5 +8,3 @@ export const auth = createAuthClient({
   baseURL: process.env.NEXT_PUBLIC_API_URL,
   plugins: [organizationClient(), magicLinkClient()],
 });
-
-console.log('Auth Client', process.env.NEXT_PUBLIC_API_URL);
